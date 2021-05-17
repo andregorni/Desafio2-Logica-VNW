142f0e7c108af4b65dc4f981f0a88ce41d671888
andregorni <ac.gorni@gmail.com>
2021-05-17 00:21:25 -0300

    Desafio 2 - Logica - VNW

---
+++ index.js
@@ -0,0 +1,46 @@
+let idade = 18
+
+if (idade >= 18) {
+    console.log("Parabéns você já completou a maioridade.")
+}
+
+
+
+let souHumano = true
+
+let isTrue = "Correto, você já completou a maioridade."
+
+if (souHumano == true) {
+    console.log(isTrue)
+}
+
+
+
+let mês = "Janeiro"
+
+if (mês == "Janeiro" || mês == "Dezembro") {
+    console.log("Parabéns pelo seu mês de aniversário")
+} else {
+    console.log("Poxa você não faz aniversário em Janeiro nem em Dezembro.")
+}
+
+
+
+let nome = "Rebrum"
+
+if (nome.charAt(0)=="R") {
+        console.log("Parabéns, o seu nome começa com a letra R.")
+    } else {
+        console.log("Poxa, o seu nome não começa com a letra R.")
+    }
+
+
+
+
+   let pessoa = "Enders"
+
+if (pessoa.charAt(0)=="E" || pessoa.length==(6) ) {
+        console.log("Parabéns, o seu nome começa com a letra E.")
+    } else {
+        console.log("Poxa, o seu nome não atendeu os requisitos.")
+    } 
